e20b929fa1110d666142941d1272635e32efa53c
gabbychoi43 <50941412+gabbychoi43@users.noreply.github.com>
2025-09-21 21:53:37 +0900

    프로젝트 데이터 삭제

--- YG1/Query/View/scm_VM_PSI_REGIONAL_SUPPLY_DATA.sql
+++
@@ -1,4 +0,0 @@
-CREATE VIEW [scm].[VM_PSI_REGIONAL_SUPPLY_DATA]
-AS
-SELECT [Area], [GRDP_005] AS 'EDP', [NEWRDD] as 'RDD', [Qty]
-FROM [DW_YG1].[scm].[TB_SCM_PSI_04_ProductionBalance_Result]
--- YG1/Query/View/scm_VW_TB_SCM_ZROUTING_CAPA.sql
+++
@@ -1,9 +0,0 @@
-CREATE VIEW scm.VW_TB_SCM_ZROUTING_CAPA
-AS
-SELECT MATNR as [material],
-ARBPL AS [Work Ctr],
-VGW01 AS [set_up_time],
-VGW02 AS [cycle_time],
-VORNR AS [Operation/Activity],
-PLNAL AS[공정 순서]
-FROM DW_YG1.scm.TB_SCM_ZROUTING
